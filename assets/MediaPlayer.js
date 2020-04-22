668689b31aea19b57feb1cb89bc9ea39d5a9e8cc
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-04-22 17:39:57 -0500

    add basic configuration autoplay plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,7 +1,13 @@
 function MediaPlayer(config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+    this._initPlugins();
 }
-
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    })
+}
 MediaPlayer.prototype.play = function() {
     this.media.play();
 }
@@ -14,6 +20,13 @@ MediaPlayer.prototype.togglePlay = function() {
     } else {
         this.pause();
     }
+}
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+}
+MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+
 }
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
 const playButton = document.querySelector('#playButton');
 
 const player = new MediaPlayer({
-    el: video
+    el: video,
+    plugins: [new AutoPlay()]
 });
 playButton.onclick = () => player.togglePlay();
