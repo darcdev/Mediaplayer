b525c88f931238e86da993e529254ae667f1e177
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-04-24 21:14:57 -0500

    change project to typescript

---
+++ assets/index.ts
@@ -0,0 +1,21 @@
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
+
+const video : HTMLElement = document.querySelector('video');
+const playButton : HTMLElement = document.querySelector('#playButton');
+const muteButton : HTMLElement = document.querySelector('#muteButton');
+
+const player = new MediaPlayer({
+    el: video,
+    plugins: [new AutoPlay(), new AutoPause()]
+});
+playButton.onclick = () => player.togglePlay();
+muteButton.onclick = () => player.toggleMute();
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.ts', ).catch(error => {
+        console.log(error.message);
+    })
+}
